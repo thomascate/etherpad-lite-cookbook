39ee898902a950a32b52d0e69905270296103553
Thomas Cate <thomascate@gmail.com>
2015-10-13 13:55:25 -0500

    hardset versions to prevent breaking changes from upstream cookbooks

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,11 @@ description      'Installs etherpad-lite'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.0'
 
-depends         'nodejs'
-depends         'database'
-depends         'postgresql'
-depends         'npm'
-depends         'nginx'
-depends         'runit'
+depends         'nodejs', '= 2.4.2'
+depends         'database', '= 4.0.9'
+depends         'postgresql', '= 3.4.16'
+depends         'npm', '= 0.1.2'
+depends         'nginx', '= 2.7.6'
+depends         'runit', '= 1.7.2'
 
 # Check README.md for attributes
